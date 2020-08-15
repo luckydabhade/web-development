6ad4eb1468b23bc2c1f4db3792a2fc7680d0788e
luckydabhade <luckydabhade>
2020-08-15 13:18:02 +0530

    edit

---
+++ mod4-solution/goodbye.js
@@ -0,0 +1,11 @@
+(function(window){
+	var byeSpeaker = {};
+
+	var speakWord = "Good Bye";
+
+	byeSpeaker.speak = function (a) {
+	  console.log(speakWord + " " + a);
+	}
+
+	window.byeSpeaker = byeSpeaker;
+})(window);	
---
+++ mod4-solution/hello.js
@@ -0,0 +1,12 @@
+(function(window){
+	var helloSpeaker = {};
+
+	var speakWord = "Hello";
+
+	helloSpeaker.speak = function (a) {
+	  console.log(speakWord + " " + a);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
